c9165dc1d4f8f6eda23abf88cd74cad6855486aa
Simran <simrandahiya004@gmail.com>
2023-10-17 18:36:36 +0530

    first commit

---
+++ chatbot/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./App/Pages/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <NavigationContainer>
+        <HomeScreenNavigation />
+      </NavigationContainer>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
